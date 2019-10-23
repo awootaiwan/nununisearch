3a64f8eff22833a2c352174e0778df536c4d7fb3
江宗祐 <zongyojiang@Zongyojiang-MNB.local>
2019-10-23 11:39:21 +0800

    init react js

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,1 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ErrorAlert from "./components/erroralert/ErrorAlert";
+// import Suggestion from "./components/suggestion/suggestion";
+// import ProductList from "./components/product/ProductList";
+// import ProductTag from "./components/productTag/productTag";
+
 import "./sidenav.js";
+
+
+const App = props => (
+  <React.Fragment>
+    {props.errcode === 0 ? (
+      props.children
+    ) : (
+      <ErrorAlert errmsg={props.errmsg} />
+    )}
+  </React.Fragment>
+);
+
+ReactDOM.render(
+  <App errcode={1} errmsg={'sdaf'}>
+    <h1>hihi</h1>
+  </App>,
+  document.getElementById('siteSearch')
+);
