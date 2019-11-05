5fbcd01b88f950d58b5a3070c49b11666315be0d
“marionLove” <marionma@awoo.com.tw>
2019-11-05 17:46:29 +0800

    style 複寫測試

--- src/components/ControlSet/DisplayMode.js
+++ src/components/ControlSet/DisplayMode.js
@@ -8,21 +8,22 @@ const DisplayMode = (props) => {
   const changeToBlockMode = () => {
     props.setBarMode('');
   }
-    return  (
-      <div className="nununi-display">
-        <div className="nununi-display-icon" onClick={changeToBlockMode}>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-          <span className="block"></span>
-        </div>
-        <div className="nununi-display-icon" onClick={changeToBarMode}>
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-        </div>
+
+  return  (
+    <div className="nununi-display">
+      <div className="nununi-display-icon" onClick={changeToBlockMode}>
+        <span className="block"></span>
+        <span className="block"></span>
+        <span className="block"></span>
+        <span className="block"></span>
       </div>
-    );
+      <div className="nununi-display-icon" onClick={changeToBarMode}>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </div>
+    </div>
+  );
 }
 
 export default DisplayMode;
